f68192da8da95e413d3cd2a84934e02845dff697
Guillaume Lambermont <guillaume.lambermont@gmail.com>
2024-05-13 10:38:03 +0200

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'petinder-angular' title`, () => {
+  it(`should have the 'PetindeRrr' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('petinder-angular');
+    expect(app.title).toEqual('PetindeRrr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, petinder-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('PetindeRrr');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'petinder-angular';
+  title = 'PetindeRrr';
 }
